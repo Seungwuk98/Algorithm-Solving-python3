ea5de9ce704f4ca9574257a59fab9c996736545b
Seungwuk98 <88603100+Seungwuk98@users.noreply.github.com>
2022-04-25 02:21:05 +0900

    learn0424

--- practice.py
+++ practice.py
@@ -1,20 +1,4 @@
-from random import randint
-from collections import defaultdict
+from math import *
 
-m = randint(1, 200000)
-qry = []
-s = defaultdict(int)
-while len(qry) < m:
-    a, b = randint(1, 3), randint(1, int(1e9))
-    if a == 1:
-        s[b] += 1
-    elif a == 2 and not s[b]:
-        continue
-    elif a == 3 and not qry:
-        continue
-    qry.append([a, b])
-
-
-print(m)
-for x in qry:
-    print(*x)
+print(exp(1))
+print(1700000000/5)
